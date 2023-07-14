690765a1d9d296b4e607d7e6f798cfa9f69286f8
a-wing <1@233.email>
2023-07-14 18:43:14 +0800

    feat(cloud): add log

--- cloud/main.go
+++ cloud/main.go
@@ -20,6 +20,21 @@ func main() {
 	}
 
 	server := lightcable.New(lightcable.DefaultConfig)
+	server.OnRoomReady(func(room string) {
+		log.Printf("Room Ready: %s", room)
+	})
+	server.OnRoomClose(func(room string) {
+		log.Printf("Room Close: %s", room)
+	})
+	server.OnConnReady(func(c *lightcable.Client) {
+		log.Printf("Room: %s, Conn Ready: %s", c.Room, c.Name)
+	})
+	server.OnConnClose(func(c *lightcable.Client) {
+		log.Printf("Room: %s, Conn Close: %s", c.Room, c.Name)
+	})
+	server.OnMessage(func(m *lightcable.Message) {
+		log.Printf("Room: %s, Conn: %s, Data: %s, Code: %d", m.Room, m.Name, m.Data, m.Code)
+	})
 	go server.Run(context.Background())
 
 	log.Println("Listen address:", *address)
