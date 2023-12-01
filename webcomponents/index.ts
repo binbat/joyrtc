fcc7267210318c2bdbb093f429c75cacc4b29a1a
zreren <70315161+zreren@users.noreply.github.com>
2023-12-01 19:26:43 +0800

    Apply suggestions from code review
    Co-authored-by: rocka <i@rocka.me>

--- webcomponents/index.ts
+++ webcomponents/index.ts
@@ -2,8 +2,8 @@
 
 import './style/index.css'
 import "./model/joy-rtc"
-
-
-
-
-
+import JoyRtcComponent from './model/joy-rtc';
+window.addEventListener('DOMContentLoaded', () => {
+  const joyrtc = window.document.getElementsByTagName('joy-rtc')[0] as JoyRtcComponent;
+  joyrtc.address = window.location.href.replace(/^http/, 'ws') + 'socket';
+})
